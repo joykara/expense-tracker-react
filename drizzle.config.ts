b4ffb85da3f67fea47eca36c9aa0c7a6ae658c98
joykara <joykarani1@gmail.com>
2025-10-13 21:35:21 +0300

    feat(drizzle): install drizzle orm for database

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+    schema: "./src/lib/db/schema.ts",
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.VITE_SUPABASE_URL!,
+    },
+} satisfies Config;

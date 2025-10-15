dc2b9751e5f6fdd58a8a91fcdfe902cbfceaae6d
joykara <joykarani1@gmail.com>
2025-10-15 13:08:31 +0300

    chore: update output directory for migrations in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "drizzle-kit";
 
 export default {
     schema: "./src/lib/db/schema.ts",
+    out: './drizzle', // Output directory for migrations
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.VITE_SUPABASE_URL!,
